427a77d25677ad9b65d2ba8def77d01a0ca49e38
Monica Tamariz <14221454+mtamariz@users.noreply.github.com>
2023-02-08 17:48:46 +0000

    Update ChiSquareAnalysis.R

--- ChiSquareAnalysis.R
+++ ChiSquareAnalysis.R
@@ -3,17 +3,16 @@ str(a)
 
 t <-  table(a$Produced_0_Expert_1_Peer)
 
-chisq.test(t)         # chi square of number of times expert's and peer's variants were produced
+# Hypothesis-testing:
+chisq.test(t)         # chi square of number of times expert's and peer's variants were produced (Hypothesis 1)
 
 t <-  table(a$TransmissionContext_0_ExpertNovice_1_PeerPeer, a$Produced_0_Expert_1_Peer)
 
-chisq.test(c(t[1],t[3]))  # chi square of expert's and peer's variants transmitted in the expert-to novice onward transmission context
-chisq.test(c(t[2],t[4]))  # chi square of expert's and peer's variants transmitted in the peer-to-peer onward transmission context
-
-chisq.test(c(t[1],t[2]))  # chi square of expert's variants transmitted in the expert-to novice and  peer-to-peer onward transmission contexts
-chisq.test(c(t[3],t[4]))  # chi square of expert's variants transmitted in the expert-to novice and  peer-to-peer onward transmission contexts
+chisq.test(c(t[1],t[3]))  # chi square of expert's and peer's variants transmitted in the expert-to novice onward transmission context (Hypothesis 2a)
+chisq.test(c(t[2],t[4]))  # chi square of expert's and peer's variants transmitted in the peer-to-peer onward transmission context (Hypothesis 2b)
 
 
+# Control variables: 
 chisq.test(table(a$Variant_0_Parity_1_Skipping))   # No significant difference between the number of times Parity and Skipping variants were produced.
 
 chisq.test(table(a$InputOrder_0_ExpertFirst_1_PeerFrist,a$Produced_0_Expert_1_Peer))  # The transmitted variant (Expert's or Peer's) was not associated with the order of learning contexts (first from an Expert or first from a Peer
